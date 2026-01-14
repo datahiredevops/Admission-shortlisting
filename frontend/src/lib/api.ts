0003037a2a00d4a4d824827bded079401aef64be
Mukesh Eswaran <mukesheswaran@Mac.lan>
2026-01-13 18:09:09 -0600

    Allow Vercel domain

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// CHANGE THIS: Use your computer's Network IP instead of localhost
-const API_URL = "http://192.168.1.166:8000"; 
-
-// Keep the cloud fallback logic for later if you want
-// const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://192.168.1.166:8000";
+// --- THE FIX ---
+// We check the Environment Variable FIRST. 
+// If it exists (like on Vercel), we use it. 
+// If it's missing (like on Localhost), we use your IP.
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://192.168.1.166:8000"; 
 
 const api = axios.create({
   baseURL: API_URL,
